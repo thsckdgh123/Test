0fe50727ffcb557617661cec1b7b9e3b8dfda057
USER <USER@test.com>
2018-04-02 12:15:38 +0900

    test1

--- calculator/src/calculator/calculator.java
+++ calculator/src/calculator/calculator.java
@@ -5,9 +5,10 @@ public class calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0, j = 0;
-		int result;
+		int result1, result2;
 		
-		result = i + j;
+		result1 = i + j;
+		result2 = i - j;
 	}
 
 }
