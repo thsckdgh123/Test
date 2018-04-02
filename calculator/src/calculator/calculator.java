85575c241cf71d4ba7cfcbd3a30eb305c9472169
USER <USER@test.com>
2018-04-02 12:08:29 +0900

    test

---
+++ calculator/src/calculator/calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0, j = 0;
+		int result;
+		
+		result = i + j;
+	}
+
+}
